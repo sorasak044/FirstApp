b88dd1066df9589d01fe4ce6ea48a5c663b5d390
sorasak044 <takfclove1@gmail.com>
2021-12-27 13:58:58 +0700

    version5

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import commonStyles from './common-styles';
+import ColumnItems from './column-items';
+import ColumnLayouts from './common-layouts';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textbold}> sorasak yawirat</Text>
-      <Text>sorasak yawirat</Text>
+    <View style={commonStyles.container}>
+      <Text style={commonStyles.textTitle}>LAYOUT</Text>
+      <Text></Text>
+      <ColumnLayouts/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,8 +26,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: '#FFFFFF',
-  },
-  textRedbold:{
-    color: 'red',
   }
-});
+});*/
